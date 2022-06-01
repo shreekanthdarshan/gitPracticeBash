ddb223c575e583b7ebca4bd89fb78272093a6b41
shreekanthdarshan <shreekanthdarshan@gmail.com>
2022-06-01 17:14:32 +0530

    message

---
+++ src/test/java/forPracticeGit/GitTest.java
@@ -0,0 +1,14 @@
+package forPracticeGit;
+
+import org.testng.annotations.Test;
+
+public class GitTest {
+	
+	@Test
+	
+	public void gitBash() {
+		System.out.println("practice");
+		System.out.println("work");
+	}
+
+}
